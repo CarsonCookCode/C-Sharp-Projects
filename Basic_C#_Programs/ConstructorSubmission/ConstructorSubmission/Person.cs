f441781bb3c7838360cb202ee0ab9e600ff727fe
Carson Cook <73494842+CarsonCookCode@users.noreply.github.com>
2022-03-24 11:10:52 -0700

    Updated

---
+++ Basic_C#_Programs/ConstructorSubmission/ConstructorSubmission/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorSubmission
+{
+    public class Person
+    {
+        // Chaining two constructors
+        public Person(string name) : this(name, 0)
+        {
+        }
+
+        public Person(string name, int age)
+        {
+            name = name;
+            age = age;
+        }
+
+        public string name { get; set; }
+        public int age { get; set; }
+    }
+}
---
+++ Basic_C#_Programs/ConstructorSubmission/ConstructorSubmission/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creation of const variable
+            const string greeting = "Hello";
+
+            // Use of var keyword
+            var person = new Person("Carson");
+
+        }
+    }
+}
